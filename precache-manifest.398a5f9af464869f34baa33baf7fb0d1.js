8b72c7564e0d28293429d66ea00b3db4b7f3cd9d
mohananagavenkat <mohananagavenkat@gmail.com>
2020-03-30 11:45:15 +0530

    Updates

---
+++ precache-manifest.398a5f9af464869f34baa33baf7fb0d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd648f10bbf9ed973bbdaef6d1463bc",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "ce640d16e556e5fe5a37",
+    "url": "/covid19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3f7805d3126e90ed2d5",
+    "url": "/covid19/static/js/2.d8bed033.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.d8bed033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce640d16e556e5fe5a37",
+    "url": "/covid19/static/js/main.d1833e9c.chunk.js"
+  },
+  {
+    "revision": "2897f32d4e7477ba260b",
+    "url": "/covid19/static/js/runtime-main.a021e852.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19/static/media/logo.5d5d9eef.svg"
+  }
+]);
